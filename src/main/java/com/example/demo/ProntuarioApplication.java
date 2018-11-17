7a674491074b06e728627d2e541ec3329f77c94d
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-11-17 20:38:43 -0300

    INIT PROJECT

---
+++ src/main/java/com/example/demo/ProntuarioApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuarioApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuarioApplication.class, args);
+	}
+}
