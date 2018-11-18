1072f472477ed4d78debf5dde8895bd7a67e5d96
geovanneoliveira <geovanneoliveira3@gmail.com>
2018-11-17 22:28:22 -0300

    [ ListAll | show | store | delete ] => Routes_Funcionario

--- src/main/java/com/example/demo/resource/FuncionarioResource.java
+++ src/main/java/com/example/demo/resource/FuncionarioResource.java
@@ -1,11 +1,22 @@
 package com.example.demo.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.model.Funcionario;
 import com.example.demo.repository.FuncionarioRepository;
@@ -21,5 +32,33 @@ public class FuncionarioResource {
 	public List<Funcionario> listar(){
 		return funcionarioRepository.findAll();
 	}
-
+	
+	@GetMapping("/show/{id}")
+	public Optional<Funcionario> show(@PathVariable Long id) {
+		return funcionarioRepository.findById(id);
+	}
+	
+	@PostMapping("/store")
+	public ResponseEntity<Funcionario> criar(@Valid @RequestBody Funcionario funcionario, HttpServletResponse response) {
+			Funcionario funcionarioSalvo = funcionarioRepository.save(funcionario);
+			
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(funcionarioSalvo.getId()).toUri();
+			response.setHeader("Location", uri.toASCIIString());
+			
+			return ResponseEntity.created(uri).body(funcionarioSalvo);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public  ResponseEntity<Optional<Funcionario>> delete(@PathVariable Long id, HttpServletResponse response){
+		Optional<Funcionario> funcionario = funcionarioRepository.findById(id);
+				
+		if(funcionario != null) {
+			funcionarioRepository.deleteById(id);
+			return ResponseEntity.accepted().body(funcionario);
+			
+		}else {
+			return ResponseEntity.badRequest().body(funcionario);
+		}
+		
+	}
 }
